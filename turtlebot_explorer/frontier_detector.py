9bb134fcdb5bca5bcf2830e3493cd3ad6433f4e5
nikiforosdev <nikstav55@gmail.com>
2025-12-15 20:16:07 +0100

    frontiers detector update

--- turtlebot_explorer/frontier_detector.py
+++ turtlebot_explorer/frontier_detector.py
@@ -9,7 +9,10 @@ from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PointStamped
 import numpy as np
-
+import math
+from sklearn.cluster import DBSCAN
+# Note: If DBSCAN is not available, you must use the iterative clustering function provided below.
+# I will use the iterative approach to avoid external dependencies.
 
 class FrontierDetector(Node):
     """
@@ -19,7 +22,7 @@ class FrontierDetector(Node):
     
     def __init__(self):
         super().__init__('frontier_detector')
-    
+        
         # Subscribe to map with proper QoS
         qos = rclpy.qos.QoSProfile(
             reliability=rclpy.qos.ReliabilityPolicy.RELIABLE,
@@ -37,6 +40,9 @@ class FrontierDetector(Node):
         self.map_info = None
         self.frontiers = []
         
+        # Parameters for clustering: Cluster distance in grid cells (CRITICAL)
+        self.cluster_dist = 5  # Max distance in cells to form a cluster
+        
         # Timer to periodically detect frontiers
         self.create_timer(1.0, self.detect_frontiers)  # 1 Hz
         
@@ -47,6 +53,59 @@ class FrontierDetector(Node):
         self.map_info = msg.info
         self.map_data = np.array(msg.data, dtype=np.int8).reshape(
             msg.info.height, msg.info.width)
+
+    def cluster_frontiers(self, raw_frontier_cells_grid):
+        """Simple iterative clustering algorithm."""
+        
+        if not raw_frontier_cells_grid:
+            return []
+
+        # Convert list of tuples to a NumPy array for efficient distance calculation
+        points = np.array(raw_frontier_cells_grid)
+        
+        # Initial cluster list: list of lists, where each inner list contains indices from `points`
+        clusters = []
+        is_clustered = np.zeros(len(points), dtype=bool)
+
+        for i in range(len(points)):
+            if is_clustered[i]:
+                continue
+            
+            # Start a new cluster with point i
+            current_cluster_indices = [i]
+            is_clustered[i] = True
+            
+            # Iteratively find neighbors within the cluster distance
+            # Use NumPy's efficient distance calculation
+            
+            # Calculate distance squared from point i to all other points
+            p1 = points[i]
+            distances_sq = np.sum((points - p1)**2, axis=1)
+            
+            # Find neighbors within the cluster distance (squared)
+            # cluster_dist_sq = self.cluster_dist * self.cluster_dist
+            cluster_dist_sq = self.cluster_dist ** 2
+
+            # Neighbors are points that are close AND have not been clustered yet
+            neighbors_indices = np.where((distances_sq < cluster_dist_sq) & (~is_clustered))[0]
+            
+            # Add all neighbors to the current cluster
+            for j in neighbors_indices:
+                current_cluster_indices.append(j)
+                is_clustered[j] = True
+            
+            clusters.append(current_cluster_indices)
+
+        # Calculate centroids of the final clusters
+        centroids = []
+        for cluster_indices in clusters:
+            cluster_points = points[cluster_indices]
+            # Centroid is the average of x and y coordinates
+            centroid_x = np.mean(cluster_points[:, 0])
+            centroid_y = np.mean(cluster_points[:, 1])
+            centroids.append((centroid_x, centroid_y))
+            
+        return centroids
     
     def detect_frontiers(self):
         """
@@ -88,47 +147,16 @@ class FrontierDetector(Node):
             return
             
         # 2. CLUSTER FRONTIER CELLS
-        # This is a simple nearest-neighbor clustering approach
-        clusters = []
-        
-        for p1_x, p1_y in raw_frontier_cells_grid:
-            found_cluster = False
-            
-            for cluster in clusters:
-                # Check if p1 is close to the centroid of this cluster
-                c_x, c_y = cluster['centroid']
-                
-                # Use grid distance squared for efficiency
-                dist_sq = (p1_x - c_x)**2 + (p1_y - c_y)**2
-                
-                if dist_sq < self.cluster_dist_sq:
-                    # Add to cluster and update centroid
-                    cluster['points'].append((p1_x, p1_y))
-                    
-                    # Update centroid (simple running average)
-                    count = len(cluster['points'])
-                    new_c_x = (c_x * (count - 1) + p1_x) / count
-                    new_c_y = (c_y * (count - 1) + p1_y) / count
-                    cluster['centroid'] = (new_c_x, new_c_y)
-                    
-                    found_cluster = True
-                    break
-            
-            if not found_cluster:
-                # Start a new cluster
-                clusters.append({
-                    'centroid': (p1_x, p1_y), 
-                    'points': [(p1_x, p1_y)]
-                })
+        # Use the corrected clustering function
+        cluster_centroids_grid = self.cluster_frontiers(raw_frontier_cells_grid)
 
         # 3. PUBLISH CENTROIDS (World Coordinates)
         self.frontiers = []
-        for cluster in clusters:
-            gx, gy = cluster['centroid']
+        for gx_float, gy_float in cluster_centroids_grid:
+            # Convert centroid grid coordinates (which are floats due to averaging) to world coordinates
+            wx = self.map_info.origin.position.x + gx_float * self.map_info.resolution + (self.map_info.resolution / 2.0)
+            wy = self.map_info.origin.position.y + gy_float * self.map_info.resolution + (self.map_info.resolution / 2.0)
             
-            # Convert centroid grid coordinates to world coordinates
-            wx = self.map_info.origin.position.x + gx * self.map_info.resolution + (self.map_info.resolution / 2.0)
-            wy = self.map_info.origin.position.y + gy * self.map_info.resolution + (self.map_info.resolution / 2.0)
             self.frontiers.append((wx, wy))
             
             # Publish point
